dd7ec30c894808b8a10f58a32756336f6587ef33
Riccardo Piccoli <riccardo.piccoli@softonic.com>
2021-05-27 12:25:30 +0200

    fix: water harvesting

--- pkg/water-harvesting/solution.go
+++ pkg/water-harvesting/solution.go
@@ -1,1 +1,44 @@
 package water_harvesting
+
+func getBucketWater(start int, in []int) (int, int) {
+	if len(in) - start < 3 {
+		return 0, len(in)
+	}
+
+	startHeight := 0
+	totalWater := make([]int, len(in))
+	lastHeight := 0
+	lastHeightPos := 0
+
+	for i := start; i < len(in); i++ {
+		if in[i] > lastHeight && startHeight > 0 {
+			lastHeight = in[i]
+			lastHeightPos = i
+		}
+		if in[i] < startHeight {
+			totalWater[i] = (startHeight - in[i])
+		} else {
+			startHeight = in[i]
+		}
+		if lastHeight > startHeight {
+			break
+		}
+	}
+	sum := 0
+	diff := startHeight - lastHeight
+	for i := start + 1; i < lastHeightPos; i++ {
+		sum += totalWater[i] - diff
+	}
+	return sum, lastHeightPos
+}
+
+func howMuchWater(in []int) int {
+	i := 0
+	var res int
+	sum := 0
+	for i < len(in) {
+		res, i = getBucketWater(i, in)
+		sum += res
+	}
+	return sum
+}
--- pkg/water-harvesting/solution_test.go
+++ pkg/water-harvesting/solution_test.go
@@ -2,6 +2,38 @@ package water_harvesting
 
 import "testing"
 
+type TestTuple struct {
+	Input []int
+	Expected int
+}
+func TestNoWater(t *testing.T) {
+	input := []int{5,0}
+	got := howMuchWater(input)
+	expected := int(0)
+	if got != expected {
+		t.Errorf("Expected %d,int32 got %d", expected, got)
+	}
+}
+
 func TestSimpleWater(t *testing.T) {
-	
+	buckets := []TestTuple{
+		{ Input: []int{5,0,5}, Expected: 5},
+		{ Input: []int{6,0,6}, Expected: 6},
+		{ Input: []int{5,2,5}, Expected: 3},
+		{ Input: []int{5,0,3}, Expected: 3},
+		{ Input: []int{5,0,0,3}, Expected: 6},
+		{ Input: []int{5,0,3,2,0}, Expected: 3},
+		{ Input: []int{5,0,3,2,0,8}, Expected: 15},
+		{ Input: []int{5,0,3,8,0,2}, Expected: 9},
+		{ Input: []int{5,0,3,2,0,2}, Expected: 5},
+		{ Input: []int{5,0,3,8,0,8,0,2}, Expected: 17},
+		{ Input: []int{5,0,5,0,5}, Expected: 10},
+	}
+	for i, input := range buckets {
+		got := howMuchWater(input.Input)
+		expected := input.Expected
+		if got != expected {
+			t.Errorf("Case %d, expected %d, got %d", i, expected, got)
+		}
+	}
 }
